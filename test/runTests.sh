a6ee050351bee9f5685ff670d4e16bd5d1426097
Hayden Pfeiffer <15hp19@queensu.ca>
2021-06-15 11:26:54 -0400

    Updated test script description

--- test/runTests.sh
+++ test/runTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 -eq "-u" || $# -ne 1 ]]; then
-   echo "Usage: runTests.sh <txl rule>. This script runs 'txl test' on all pt files in the passed in directory, and stores outputs as *.eoutput. This script must be run from inside local ptsrc (project root) dir. For now, the immediate output files are in project root."
+   echo "Usage: runTests.sh <txl rule>. This script runs 'txl test' on all pt files in the passed in directory, and stores outputs as test/output/temp-<filename>. This script must be run from inside local project root dir."
 
 else
     for FILE in test/Txl/*.txl; do
